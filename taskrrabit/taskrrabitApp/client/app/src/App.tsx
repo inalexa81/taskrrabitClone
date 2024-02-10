630a5a05cc55c3ce04921e8bfc45b65d1be03f85
inalexa81 <athegreat5@hotmail.com>
2024-02-10 19:14:14 +0200

    update

--- taskrrabit/taskrrabitApp/client/app/src/App.tsx
+++ taskrrabit/taskrrabitApp/client/app/src/App.tsx
@@ -5,20 +5,14 @@ import './App.scss'
 import Users from './components/Users'
 import { Home } from './pages/Home'
 
-// App.js or any component that wraps your routes
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <Router>
-      <div style={{ height: '100vh', backgroundImage: `url("https://www.shutterstock.com/image-photo/handyman-tools-260nw-264151898.jpg")`, backgroundSize: 'cover', }}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {/* Add more routes here if needed */}
-        </Switch>
-      </div>
-    </Router>
+<>
+  <Home/>
+</>
   );
 }
 
